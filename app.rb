60315a60867e8072ef7a043b63e2d852cad601cb
risako070310 <risako070310@gmail.com>
2021-09-01 05:43:36 +0000

    use session

--- app.rb
+++ app.rb
@@ -1,3 +1,13 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+
+enable :sessions
+
+get '/' do
+    session[:test]
+end
+
+get '/input' do
+    session[:test] = "Hello World!"
+end
