bae55fa291dd1a2bc73abc8eb9e7f16052e5872a
risako070310 <risako070310@gmail.com>
2021-09-01 06:21:12 +0000

    add validation

--- models.rb
+++ models.rb
@@ -2,4 +2,10 @@ ActiveRecord::Base.establish_connection
 
 class User < ActiveRecord::Base
     has_secure_password
+    validates :mail,
+        presence: true,
+        format: {with:/\A.+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+\z/}
+    validates :password,
+        format: {with:/(?=.*?[a-z])(?=.*?[0-9])/},
+        length: {in: 8..16}
 end
